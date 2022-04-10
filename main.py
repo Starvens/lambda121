243886ab03e75a37fcd2e1fcf1351b70bd72fa6f
Starvens <100984010+Starvens@users.noreply.github.com>
2022-04-09 22:37:07 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,94 @@
+import json
+import os
+
+import pymongo
+import random
+import boto3
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+import datetime
+
+os.environ['TZ'] = 'US/Eastern'
+# mongo setup
+CONNECTION_STRING = "mongodb+srv://awsLambda:vQ1TkTcAgL9z8jHK@starvvenssimplefileshar.ruwbw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+s3_client = boto3.client('s3')
+myclient = pymongo.MongoClient(CONNECTION_STRING)
+mydb = myclient["starvens"]
+mycol = mydb["uniqueUrls"]
+
+
+def generateUniqueURL():
+    required = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~:@!$*='
+    maxLength = 35
+    totLen = len(required)
+    ans = []
+    for i in range(maxLength):
+        x = random.randint(0, totLen - 1)
+        ans.append(required[x])
+    return ''.join(ans)
+
+
+def checkIfUrlExistInMongo(url):
+    logger.info('came to method')
+    records = mycol.find_one({"uniqueUrl": url})
+    records = records if records else []
+    for _ in records:
+        logger.info('already in db')
+        return True
+    return False
+
+
+def lambda_handler(event, context):
+    try:
+        # get the file name
+        logger.info(str(event))
+        # event = json.loads(event)
+        # tempBody = event['body']
+        # fileName = tempBody['fileName']
+        # ip = tempBody['ipAddress']
+        tempBody = event['body']
+        tempBody = json.loads(tempBody)
+        fileName = tempBody['fileName']
+        ip = tempBody['ipAddress']
+        logger.info(f'{fileName}{ip}')
+
+        while True:
+            curUrl = generateUniqueURL()
+            logger.info(curUrl)
+            if not checkIfUrlExistInMongo(curUrl):
+                logger.info('breaked')
+                break
+            logger.info('should not be here')
+        logger.info(f'generated url :: {curUrl}')
+
+        # TODO
+        rowInDb = {
+            "uniqueUrl": curUrl,
+            "fileName": fileName,
+            "isFileUploadedToS3": False,
+            "ipAddress": ip,
+            "cratedAt": str(datetime.datetime.now())
+        }
+        logger.info(f'data need to be inserted : {str(rowInDb)}')
+
+        # generate presigned url
+        # TODO expiration time
+        # response = s3_client.generate_presigned_url('put_object', Params={'Bucket': 'starvensdriveguest', 'Key': f'{curUrl}/{fileName}'})
+        response = s3_client.generate_presigned_url('put_object', Params={'Bucket': 'starvensdriveguest', 'Key': f'{curUrl}/{fileName}', 'ContentType': 'video/mp4'})
+
+        mycol.insert_one(rowInDb)
+        logger.info('successfully inserted into mongo')
+
+        temp = {'presignedUrl': response}
+
+        # x = mycol.insert_one(mydict)
+        return {
+            'statusCode': 200,
+            # 'body': json.dumps(event)
+            'body': json.dumps(temp)
+        }
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
